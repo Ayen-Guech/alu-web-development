4ff4eb00954b771ab1651eb3f1dbaca81292fc2f
Ayen-Athiak <ayenathiak@gmail.com>
2025-01-17 14:17:43 +0200

    task

--- caching/4-mru_cache.py
+++ caching/4-mru_cache.py
@@ -46,6 +46,8 @@ class MRUCache(BaseCaching):
             self._remove(key)
             self._add(key, value)
             return value
+        
+        
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
 
         
