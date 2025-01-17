0701a5e0df785a66c25e8358ef1293da4a94e263
Ayen-Athiak <ayenathiak@gmail.com>
2025-01-17 13:49:53 +0200

    caching in python

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""  the bases of  dictionary """
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ This class inherits from BaseCaching and is a caching system
+        AND THE caching system doesn’t have limit """
+
+    def put(self, key, item):
+        """ Assigning the dictionary """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value  """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
